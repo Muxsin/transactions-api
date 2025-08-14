70d6ef71e7fa60150ebd709a9903e032a8395ed6
Muxsin <muxsin.shokirov@gmail.com>
2025-08-14 19:27:48 +0700

    created method for generating random address

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"crypto/rand"
+	"encoding/hex"
+)
+
+func generateRandomAddress() (string, error) {
+	bytes := make([]byte, 32)
+	if _, err := rand.Read(bytes); err != nil {
+		return "", err
+	}
+
+	return hex.EncodeToString(bytes), nil
+}
+
 func main() {
 
 }
