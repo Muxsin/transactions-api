b12042c421166affbfa0efaf34401288c2afbfb9
Muxsin <muxsin.shokirov@gmail.com>
2025-08-14 18:27:15 +0700

    added graceful shutdown

--- cmd/transactions-api/main.go
+++ cmd/transactions-api/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
 	"infotecs-transactions-api/internal/app"
 	"infotecs-transactions-api/internal/config"
 	"infotecs-transactions-api/internal/database"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -19,7 +25,25 @@ func main() {
 	}
 
 	myApp := app.New(config.New(), db)
-	if err := myApp.Run(); err != nil {
-		panic(err)
+
+	stopCh := make(chan os.Signal, 1)
+	signal.Notify(stopCh, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		if err := myApp.Run(); err != nil {
+			panic(err)
+		}
+	}()
+
+	sig := <-stopCh
+	log.Printf("Received signal: %v. Starting graceful shutdown...", sig)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := myApp.Shutdown(ctx); err != nil {
+		log.Fatalf("Graceful shutdown failed: %v", err)
 	}
+
+	log.Println("Application shutdown gracefully. Exiting.")
 }
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,7 +1,10 @@
 package app
 
 import (
+	"context"
+	"fmt"
 	"infotecs-transactions-api/internal/config"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -11,6 +14,7 @@ type app struct {
 	config *config.Config
 	router *gin.Engine
 	db     *gorm.DB
+	server *http.Server
 }
 
 func New(config *config.Config, db *gorm.DB) *app {
@@ -20,5 +24,27 @@ func New(config *config.Config, db *gorm.DB) *app {
 	}
 	app.router = app.loadRoutes()
 
+	app.server = &http.Server{
+		Addr:    fmt.Sprintf(":%s", app.config.HTTPServerPort),
+		Handler: app.router,
+	}
+
 	return app
+}
+
+func (a *app) Shutdown(ctx context.Context) error {
+	if err := a.server.Shutdown(ctx); err != nil {
+		return fmt.Errorf("server shutdown failed: %w", err)
+	}
+
+	db, err := a.db.DB()
+	if err != nil {
+		return fmt.Errorf("failed to get database instance: %w", err)
+	}
+
+	if err := db.Close(); err != nil {
+		return fmt.Errorf("failed to close database connection: %w", err)
+	}
+
+	return nil
 }
