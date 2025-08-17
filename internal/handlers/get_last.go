7fde41a6c485803b1a71cf6567cab680c0f95052
Muxsin <muxsin.shokirov@gmail.com>
2025-08-17 15:39:19 +0700

    changed get last transaction response

--- internal/handlers/get_last.go
+++ internal/handlers/get_last.go
@@ -1,11 +1,21 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
+
+type transactionResponse struct {
+	Id        uint   `json:"id"`
+	From      string `json:"from"`
+	To        string `json:"to"`
+	Amount    string `json:"balance"`
+	CreatedAt string `json:"created_at"`
+}
 
 func (h *handler) GetLast(c *gin.Context) {
 	count, err := strconv.Atoi(c.DefaultQuery("count", "10"))
@@ -20,5 +30,17 @@ func (h *handler) GetLast(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, transactions)
+	var response []transactionResponse
+
+	for _, transaction := range transactions {
+		response = append(response, transactionResponse{
+			Id:        transaction.ID,
+			From:      transaction.From,
+			To:        transaction.To,
+			Amount:    fmt.Sprintf("%.2f", float64(transaction.Amount)/100),
+			CreatedAt: transaction.CreatedAt.Format(time.RFC3339),
+		})
+	}
+
+	c.JSON(http.StatusOK, response)
 }
